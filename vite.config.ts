e8fdc66713aa2f8d090fe6b3fb32a8d8c5a9871c
Blake Harley <blake@blakeharley.com>
2026-01-18 18:13:34 -0800

    Attempt to get PWA to work offline

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [VitePWA({
     registerType: 'autoUpdate',
-    injectRegister: false,
+    injectRegister: 'auto',
 
     manifest: {
       name: 'Precision Clock',
